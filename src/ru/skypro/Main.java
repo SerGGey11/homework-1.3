c4a9734e78c95d22569689aa1cb3bd03fcaa455d
SerGGey <-1010>
2022-01-05 12:01:22 +0300

    попытка на дз

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,76 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int clientOs = 0;
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Андроид");
+        } else if (clientOs == 0) ;
+        {
+            System.out.println("Установите версию прижения для IOS");
+        }
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear > 2015) {
+            System.out.println("Приложение будет работать корректно");
+        } else if (clientDeviceYear < 2015) ;
+        {
+            System.out.println("Советуем установить новую облегчённую версию для IOS по ссылке");
+        }
+        int year = 2021;
+        int daysInYear = 365;
+        if (daysInYear == 366) ;
+        {
+            System.out.println("2021 не год является високосным");
+        }
+        int deliveryDistance = 95;
+        if (deliveryDistance > 60 && deliveryDistance < 100) ;
+        System.out.println("Курьерская доставка составит трое суток");
     }
-}
+    int mounthInYear = 12;
+    if(mounthInYear == 1 );{
+        System.out.println("Январь");{
+        else if (mounthInYear == 2);
+            System.out.println("Февраль");
+            if(mounthInYear == 3);
+            System.out.println("Март");
+            if(mounthInYear == 4);
+            System.out.println("Апрель");
+            if(mounthInYear == 5);
+            System.out.println("Май");
+            if(mounthInYear == 6);
+            System.out.println("Июнь");
+            if(mounthInYear == 7);
+            System.out.println("Июль");
+            if(mounthInYear == 8);
+            System.out.println("Август");
+            if(mounthInYear == 9);
+            System.out.println("Сентябрь");
+            if(mounthInYear == 10);
+            System.out.println("Октябрь");
+            if(mounthInYear == 11);
+            System.out.println("Ноябрь");
+            if(mounthInYear == 12);
+            System.out.println("Декабрь");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
