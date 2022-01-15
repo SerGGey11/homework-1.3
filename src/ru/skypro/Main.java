a6590ad90154e5a9bc9c4ab238abb9119b32f217
SerGGey <-1010>
2022-01-15 15:17:48 +0300

    попытка на дз

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -17,62 +17,54 @@ public class Main {
         {
             System.out.println("Советуем установить новую облегчённую версию для IOS по ссылке");
         }
+
         int year = 2021;
-        int daysInYear = 365;
-        if (daysInYear == 366) ;
         {
             System.out.println("2021 не год является високосным");
         }
+
         int deliveryDistance = 95;
         if (deliveryDistance > 60 && deliveryDistance < 100) ;
         System.out.println("Курьерская доставка составит трое суток");
-    }
-    int mounthInYear = 12;
-    if(mounthInYear == 1 );{
-        System.out.println("Январь");{
-        else if (mounthInYear == 2);
-            System.out.println("Февраль");
-            if(mounthInYear == 3);
-            System.out.println("Март");
-            if(mounthInYear == 4);
-            System.out.println("Апрель");
-            if(mounthInYear == 5);
-            System.out.println("Май");
-            if(mounthInYear == 6);
-            System.out.println("Июнь");
-            if(mounthInYear == 7);
-            System.out.println("Июль");
-            if(mounthInYear == 8);
-            System.out.println("Август");
-            if(mounthInYear == 9);
-            System.out.println("Сентябрь");
-            if(mounthInYear == 10);
-            System.out.println("Октябрь");
-            if(mounthInYear == 11);
-            System.out.println("Ноябрь");
-            if(mounthInYear == 12);
-            System.out.println("Декабрь");
-
-    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
+        int mounthInYear = 12;
+        switch (mounthInYear) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
